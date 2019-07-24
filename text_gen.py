d9524b433c59eb34bd027bacdac3a6f031ccb998
MasonCaiby <maxwell.caudle@gmail.com>
2019-07-24 08:22:37 -0600

    model workin add text gen after each epoch next

--- text_gen.py
+++ text_gen.py
@@ -4,12 +4,14 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout, LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
+import os
 
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
 
 class TextGenChar:
-    def __init__(self, text_file, model_dir, num_chars=40, seq_length=50, email_on=False):
+    def __init__(self, text_file, num_chars=40, seq_length=50, email_on=False):
         self.text_file = text_file
-        self.model_dir = model_dir
+
         self.num_chars = num_chars
         self.email_on = email_on
         self.seq_length = seq_length
@@ -47,31 +49,47 @@ class TextGenChar:
             self.X.append(sequence)
             self.Y.append(label)
 
-        self.X_m = np.reshape(X, (len(X), self.seq_length, 1))
-        self.X_m /= float(len(self.n_char))
+        self.X_m = np.reshape(self.X, (len(self.X), self.seq_length, 1))
+        self.X_m = self.X_m / float(len(self.n_char))
         self.Y_m = np_utils.to_categorical(self.Y)
 
 
 class textGenModel:
-    def __init__(self, text_obj):
+    def __init__(self, text_obj, model_dir):
         self.text_obj = text_obj
+        self.model_dir = model_dir
 
         self.model = Sequential()
 
     def build_model(self):
         self.model.add(LSTM(400,
                             input_shape=(self.text_obj.X_m.shape[1],
-                                         self.text_obj.seq_length),
+                                         self.text_obj.X_m.shape[2]),
                             return_sequences=True))
         self.model.add(Dropout(0.2))
         self.model.add(LSTM(400))
         self.model.add(Dropout(0.2))
         self.model.add(Dense(self.text_obj.Y_m.shape[1], activation='softmax'))
-        self.model.compile(loss='categorical_crossentorpy', optimizer='adam')
+        self.model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-    def
+    def train_model(self):
+        for epoch in range(50):
+            filepath = self.model_dir+"model_{}.hdf5".format(epoch)
+
+            checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1,
+                                         save_best_only=False, mode='min')
+            callbacks_list = [checkpoint]
+
+
+            hista = self.model.fit(self.text_obj.X_m, self.text_obj.Y_m,
+                              verbose=1, epochs=1, batch_size=128,
+                              callbacks=callbacks_list)
+
 
 if __name__ == "__main__":
-    text = TextGenChar("data/three_musketers.txt", "model")
+    text = TextGenChar("data/three_musketers.txt")
     text.load_text()
     text.pre_processsing()
+    model = textGenModel(text_obj=text, model_dir="three_musk_model/")
+    model.build_model()
+    model.train_model()
